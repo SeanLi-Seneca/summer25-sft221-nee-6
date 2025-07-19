b14364001f2389a690c0589d83b9fdb80ed7a6be
sahannn23 <svhewa-gallage@myseneca.ca>
2025-07-19 02:12:01 -0400

    MS4 updated code

--- SourceCode/shipment_functions.c
+++ SourceCode/shipment_functions.c
@@ -45,4 +45,24 @@ bool validateDestination(const char map[25][25], const struct Point* dest) {
     return map[dest->row][dest->col] == 1; // 1 = valid, 0 = building
 }
 
+int calculateRouteDistance(const char map[25][25], struct Point start, struct Point end) {
+    // Manhattan distance (placeholder for real routing logic)
+    return abs(start.row - end.row) + abs(start.col - end.col);
+}
 
+int findTruckForShipment(const char map[25][25], struct Truck trucks[], int numTrucks, const struct Shipment* shipment) {
+    int bestIndex = -1;
+    int minDistance = 1000000; // Arbitrary large value
+
+    for (int i = 0; i < numTrucks; i++) {
+        if (canHandleShipment(&trucks[i], shipment)) {
+            int distance = calculateRouteDistance(map, trucks[i].location, shipment->origin);
+            if (distance < minDistance) {
+                minDistance = distance;
+                bestIndex = i;
+            }
+        }
+    }
+
+    return bestIndex;
+}
