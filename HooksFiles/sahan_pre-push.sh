aee036e36f5edfeed5fe7c5f5b8cd88d69dab0ef
sahannn23 <svhewa-gallage@myseneca.ca>
2025-07-28 22:05:09 -0400

    Add files via upload

--- HooksFiles/sahan_pre-push.sh
+++ HooksFiles/sahan_pre-push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 echo "Starting test runner..." 
 test_runner="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" 
-tests_dll="C:\Canada-Job\Seneca\7-Winter 2025\SFT221-NGG\Week 9\VersionControlDemo\SourceCode\Project1\x64\Debug\UnitTest1.dll" 
+tests_dll="C:\Seneca\SFT221\MS4\SourceCode\Project1\x64\Debug\UnitTest1.dll" 
 failure_string="Failed:" 
 testout=`"$test_runner" "$tests_dll"` 
 if [[ $testout == *"$failure_string"* ]]; then 
